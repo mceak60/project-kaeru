9184ffea0d1e3cfd842cb088df5e383865331f70
Nick Gonzalez <mceak60@gmail.com>
2021-01-24 15:34:26 -0600

    Revert "Merge branch 'main' into FMAB_OP5_Brennan"
    This reverts commit 422cbe552bda3a8a95e1a8a50ee79c00be9d7750, reversing
    changes made to b40eb77cc18033ad55863c946e8d10d74bce734f.

--- ProjectKaeru/Assets/Scripts/PlayerDeath.cs
+++ ProjectKaeru/Assets/Scripts/PlayerDeath.cs
@@ -6,21 +6,22 @@ public class PlayerDeath : MonoBehaviour
 {
     private bool isDead = false;
 
-    //Nick commented out this code because he thought it might have caused the shadow clone glitch but I don't think thats the case and we may need to update it and use it sometime -Bren
-    /*private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (collision.gameObject.CompareTag("Death"))
+        if (!isDead)
         {
-            Destroy(gameObject);
-            LevelManager.instance.Respawn();
+            if (col.gameObject.CompareTag("Death"))
+            {
+                isDead = true;
+                Destroy(gameObject);
+                LevelManager.instance.Respawn();
+            }
         }
-    }*/
+    }
 
     /*
      * Whenever the player enters a trigger with the "Death" tag the player is deleted and a new prefab is created at the respawn point
      * Max makes a good point that it may be better to just teleport the player to the respawn point instead and I agree since deleting it and making a new prefab would reset the player's health completely -Bren
-     * 
-     * I did the teleport to respawn change, you're welcome -Nick
      */
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -29,8 +30,8 @@ public class PlayerDeath : MonoBehaviour
             if (col.gameObject.CompareTag("Death"))
             {
                 isDead = true;
+                Destroy(gameObject);
                 LevelManager.instance.Respawn();
-                isDead = false;
             }
         }
     }
