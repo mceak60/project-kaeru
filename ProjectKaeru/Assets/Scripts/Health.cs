e2ebc5fc4d247baa21b38329dcfb2f722624b921
Nick Gonzalez <mceak60@gmail.com>
2021-01-26 04:13:18 -0600

    Added Health Bar
    Made the UI a component of the player prefab, hopefully that doesn't break anything

---
+++ ProjectKaeru/Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(health > numHearts)
+            {
+                health = numHearts;
+            }
+
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if(i < numHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
