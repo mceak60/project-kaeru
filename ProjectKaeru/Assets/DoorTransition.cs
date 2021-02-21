1eca4385d9ba9a11a1b835c0747bc4354780dd52
Brennan <brennanschlittler@gmail.com>
2021-02-21 14:30:50 -0600

    Implemented Brennan's Moving Rectangle

---
+++ ProjectKaeru/Assets/DoorTransition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTransition : MonoBehaviour
+{
+	private LevelLoader levelLoad;
+	public string entryPoint = "";
+	private bool canEnter = false;
+
+	private void Start()
+	{
+		levelLoad = Object.FindObjectOfType<LevelLoader>();
+	}
+
+	void Update()
+	{
+		if (canEnter && Input.GetButtonDown("Enter"))
+		{
+			levelLoad.LoadNextLevel(this.name, entryPoint);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.layer == 8)
+		{
+			canEnter = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.layer == 8)
+		{
+			canEnter = false;
+		}
+	}
+}
