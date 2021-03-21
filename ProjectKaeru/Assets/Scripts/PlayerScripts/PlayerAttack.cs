7c4feb5fc9d56898554f6704a08efb2d14c7234c
Brennan <brennanschlittler@gmail.com>
2021-03-21 14:48:25 -0500

    I don't think ive broken anyhting yet

---
+++ ProjectKaeru/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+    public float attackRate = 1f;
+    float nextAttackTime = 0f;
+    public bool isAttacking = false;
+
+    /*
+     * If the player's attack is not on cooldown then see if the player has pressed the attack button and switch to the attacking animation
+     * This code needs to be updated to check if the player is already preforming an action such as dashing or wall grabbing and if so, it should not let them attack -Bren
+     */
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            isAttacking = false;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                nextAttackTime = Time.time + 1f / attackRate;
+                isAttacking = true;
+            }
+            //Play animation
+            animator.SetBool("IsAttacking", isAttacking);
+        }
+    }
+}
---
+++ ProjectKaeru/Assets/Scripts/PlayerScripts/PlayerHitbox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitbox : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            col.gameObject.GetComponent<Enemy>().TakeDamage(40, rb);
+        }
+    }
+}
